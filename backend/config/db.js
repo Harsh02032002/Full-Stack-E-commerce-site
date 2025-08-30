4771dbfc83f0828e4a848fbe594eb0d49f64dbe9
ITFields <harshdeepbca503@gmail.com>
2025-08-30 12:16:32 +0530

    env backend commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
 async function connectDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGODB_URI);
   } catch (err) {
-    console.error("❌ MongoDB connection error:", err.message);
-    process.exit(1); // Exit with failure
+    console.log(err);
   }
 }
 
